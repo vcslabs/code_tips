046eac671c84adc898487a6f1e016ef2758fe5bb
vcslabs <taiyo8901@gmail.com>
2022-08-27 23:52:54 +0900

    feat: spring

---
+++ src/main/java/com/example/code_tips/CodeTipsApplication.java
@@ -0,0 +1,13 @@
+package com.example.code_tips;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeTipsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeTipsApplication.class, args);
+    }
+
+}
